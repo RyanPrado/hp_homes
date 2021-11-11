aa555f46a7c8635a6802c7c49b1ee140ac9bdf31
Harry Prado <67030727+RyanPrado@users.noreply.github.com>
2021-11-11 13:27:36 -0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,36 @@
+fx_version 'cerulean'
+game 'gta5'
+
+
+ui_page "web-side/nui/index.html"
+
+author 'Harry <HarryKillerBR#4560> <discord.gg/FqhvHEg>'
+description 'Codigo de Sistema de Casas'
+version '0.1.0'
+
+files {
+    "web-side/nui/**/*"
+}
+
+shared_scripts {
+    "@vrp/lib/utils.lua",
+}
+
+client_scripts {
+    "c_utils.lua",
+    "cfg/config.lua",
+    "web-side/client.lua",
+    "client-side/**/*.lua"
+}
+
+server_scripts {
+    "s_utils.lua",
+    "cfg/**/*.lua",
+    "server-side/**/*.lua"
+}
+
+
+
+shared_scripts {
+    'shared-side/**/*.lua',
+}
