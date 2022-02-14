b6ed241fa7729b3131c03db5b2d695309c3fb3c3
Harry Prado <67030727+RyanPrado@users.noreply.github.com>
2022-02-14 12:27:50 -0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,7 +7,7 @@ ui_page "web-side/nui/index.html"
 author 'Harry <HarryKillerBR#4560> <discord.gg/FqhvHEg>'
 description 'Codigo de Sistema de Casas'
 lua54 'yes'
-version '0.5.8'
+version '0.6.4'
 
 files {
     "web-side/nui/**/*",
